e19cf90f30816a85cf18f05c5bf37e16f27f80b3
jzz <jzz@1mdata.com>
2019-05-21 14:18:13 +0800

    新建XiXi类

---
+++ hello/src/XiXi.java
@@ -0,0 +1,9 @@
+/**
+ * Description:
+ * Created by jiangzz on 2019/5/21.
+ */
+public class XiXi {
+    public static void main(String[] args) {
+        System.out.println("欢迎来到 github的世界");
+    }
+}
